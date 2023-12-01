e2262ab078dc09f2ba503ef4581ce40a6da3f4c4
kodidasu43 <sai.kishore14375@gmail.com>
2023-12-01 22:53:48 +0530

    nested dictionary demo and program

---
+++ nested_dict_demo.py
@@ -0,0 +1,32 @@
+employees = {
+    "Alice": {
+        "name": "Alice Doe",
+        "department": "Marketing",
+        "role": "Marketing Manager"
+    },
+    "Bob": {
+        "name": "Bob Smith",
+        "department": "Engineering",
+        "role": "Software Engineer"
+    },
+}
+
+employees_company_y = [
+    {
+        "name": "Alice Doe",
+        "department": "Marketing",
+        "role": "Marketing Manager",
+        "skills": ["Social Media", "Content Marketing", "Email Marketing"]
+    },
+    {
+        "name": "Bob Smith",
+        "department": "Engineering",
+        "role": "Software Engineer",
+        "skills": ["Python", "JavaScript", "Java"]
+    }
+
+]
+print(employees)
+print(type(employees))
+print(employees_company_y)
+print(type(employees_company_y))
